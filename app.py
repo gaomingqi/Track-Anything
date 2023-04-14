db34462dbedaa50a61e52356c1bd0a3c8f935fa3
ShangGaoG <12132332@mail.sustech.edu.cn>
2023-04-14 15:11:24 +0800

    gao

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ xmem_checkpoint = download_checkpoint(xmem_checkpoint_url, folder, xmem_checkpoi
 
 # args, defined in track_anything.py
 args = parse_augment()
-args.port=12212
+args.port=33333
 
 model = TrackingAnything(SAM_checkpoint, xmem_checkpoint, args)
 
